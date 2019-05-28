d61c1088bd746a5d92e422b73d834a407b9d9d58
gimx <einkauf.cn@gmail.com>
2019-05-28 21:56:25 +0100

    changed to support S0 pulse input

--- firmware/open_evse/EnergyMeter.cpp
+++ firmware/open_evse/EnergyMeter.cpp
@@ -1,20 +1,47 @@
+#include <EnableInterrupt.h>
+
 #include "open_evse.h"
 
 #ifdef KWH_RECORDING
 
+//S0 pulse counter
+volatile unsigned long pulseCount = 0, pulseCountSession = 0;
+volatile unsigned long pulsetime=0;     // holds time of occurence of interrupt 
+//S0 pulse counter input pin, connect first S0 meter ouput to this pin, second S0 meter output goes to ground
+#define S0_PULSE_PIN 6
+
 EnergyMeter g_EnergyMeter;
 
+// ISR runs each time S0 pulse is detected
+void onS0Pulse(){
+  unsigned long now = millis();
+
+  //only count energy while charging
+  if(g_EvseController.GetState() == EVSE_STATE_C){   
+    pulseCount++;  
+    
+    //calculate current from time between pulses (assumption 1000imp/kWh)
+    g_EvseController.SetChargingCurrent(3600000/((now-pulsetime)) * 1000/VOLTS_FOR_L2); 
+  }
+  
+  pulsetime=now;
+}
 
 EnergyMeter::EnergyMeter()
 {
+  // S0 pulse counter output is usually open connector, hence pull-up
+  pinMode(S0_PULSE_PIN, INPUT_PULLUP);
+  // trigger on falling edge of pulse
+  enableInterrupt(S0_PULSE_PIN | PINCHANGEINTERRUPT, onS0Pulse, FALLING);     
+
   m_bFlags = 0;
   m_wattSeconds = 0;
+  m_lastUpdateMs = millis();
+  m_wattHoursTot = eeprom_read_dword((uint32_t*)EOFS_KWH_ACCUMULATED);        // get the stored value for the kWh from eeprom
 
   if (eeprom_read_dword((uint32_t*)EOFS_KWH_ACCUMULATED) == 0xffffffff) { // Check for unitialized eeprom condition so it can begin at 0kWh
     eeprom_write_dword((uint32_t*)EOFS_KWH_ACCUMULATED,0); //  Set the four bytes to zero just once in the case of unitialized eeprom
   }
-  
-  m_wattHoursTot = eeprom_read_dword((uint32_t*)EOFS_KWH_ACCUMULATED);        // get the stored value for the kWh from eeprom
 }
 
 void EnergyMeter::Update()
@@ -31,23 +58,10 @@ void EnergyMeter::Update()
     endSession();
   }
 
-  if (inSession()) {
-    uint8_t relayclosed = g_EvseController.RelayIsClosed();
-
-    
-    if (relayclosed) {
-      if (!relayClosed()) {
-	// relay just closed - don't calc, just reset timer
-	m_lastUpdateMs = millis();
-      }
-      else {
-	calcUsage();
-      }
-    }
-
-    if (relayclosed) setRelayClosed();
-    else clrRelayClosed();
-  }
+//  if (inSession()) {
+//	  m_lastUpdateMs = millis();
+	  calcUsage();
+//  }
 
   if (evconnected) setEvConnected();
   else clrEvConnected();
@@ -58,18 +72,16 @@ void EnergyMeter::calcUsage()
   unsigned long curms = millis();
   unsigned long dms = curms - m_lastUpdateMs;
   if (dms > KWH_CALC_INTERVAL_MS) {
-      uint32_t ma = g_EvseController.GetChargingCurrent();
-#ifdef VOLTMETER
-      m_wattSeconds += ((g_EvseController.GetVoltage()/1000UL) * (ma/1000UL) * dms) / 1000UL;
-#else // !VOLTMETER
-#ifdef THREEPHASE //Multiple L1 current by the square root of 3 to get 3-phase energy
-      m_wattSeconds += (((g_EvseController.GetCurSvcLevel() == 2) ? VOLTS_FOR_L2:VOLTS_FOR_L1) * (ma/1000UL) * dms * 3) / 1000UL;
-#else // !THREEPHASE
-     m_wattSeconds += (((g_EvseController.GetCurSvcLevel() == 2) ? VOLTS_FOR_L2:VOLTS_FOR_L1) * (ma/1000UL) * dms) / 1000UL;
-#endif // THREEPHASE
-#endif // VOLTMETER
-
+      // no need to bother with voltage, we have energy measured directly
+      // accumulate Watt Seconds for charging (scaled for 1000imp/kWh = 1 imp/Wh = 3600imp/Ws), adjust if necessary
+      m_wattSeconds =  pulseCount*3600;  
       m_lastUpdateMs = curms;
+  }
+  if ((g_EvseController.GetState() != EVSE_STATE_C) && pulseCount) {
+    m_wattHoursTot += pulseCount; //scaled for 1000imp/kWh = 1 imp/Wh = 3600imp/Ws, adjust if necessary
+    eeprom_write_dword((uint32_t*)EOFS_KWH_ACCUMULATED,m_wattHoursTot);
+    pulseCount = 0;    
+    g_EvseController.SetChargingCurrent(0);
   }
 }
 
@@ -85,10 +97,7 @@ void EnergyMeter::endSession()
 {
   if (inSession()) {
     clrInSession();
-    if (m_wattSeconds) {
-    m_wattHoursTot += (m_wattSeconds / 3600UL);
-      SaveTotkWh();
-    }
+    
   }
 }
 
@@ -98,4 +107,3 @@ void EnergyMeter::SaveTotkWh()
 }
 
 #endif // KWH_RECORDING
-
